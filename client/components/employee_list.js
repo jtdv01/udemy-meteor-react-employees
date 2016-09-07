2e7902b445cfb0d42ac2f900da73052caca86dd4
j <j@j.com>
2016-09-08 00:12:35 +1000

    Load moar works

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -3,21 +3,42 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
-const EmployeeList = (props) => {
-  // console.log(props.employees);
+const PER_PAGE = 20;
 
-  return(
-    <div>
-      <div className="employee-list">
-        {props.employees.map(e => <EmployeeDetail key={e._id} employee={e}/>)}
+class EmployeeList extends React.Component{
+
+  componentWillMount(){
+    this.page = 1;
+  }
+
+  handleButtonClick(){
+    Meteor.subscribe('employees',PER_PAGE * (this.page + 1) ); // loads the first 40
+    this.page += 1;
+  }
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="employee-list">
+          {this.props.employees.map(e => <EmployeeDetail key={e._id} employee={e}/>)}
+        </div>
+
+        <button onClick={this.handleButtonClick.bind(this)}
+          className="btn btn-primary">Load moar...
+        </button>
+
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default createContainer(() =>{
   // set up subscritpion
-  Meteor.subscribe('employees');
+  Meteor.subscribe('employees',PER_PAGE);
 
   // return an object
   return{ employees: Employees.find({}).fetch() };
--- server/main.js
+++ server/main.js
@@ -19,8 +19,8 @@ Meteor.startup(() => {
     });
   }
 
-  Meteor.publish("employees",function(){
-    return Employees.find({},{limit: 20});
+  Meteor.publish("employees",function(per_page){
+    return Employees.find({},{limit: per_page});
   })
 
 });
